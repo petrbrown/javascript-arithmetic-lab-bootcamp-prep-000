e9f1ca8e905cd98475bab072fefea651af19089c
petr brown <petrbrown@gmail.com>
2018-01-23 06:01:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(n,nn) {
+  return parseFloat(n)+parseFloat(nn)
+}
+function subtract(n,nn) {
+  return parseFloat(n)-parseFloat(nn)
+}
+function multiply(n,nn) {
+  return parseFloat(n)*parseFloat(nn)
+}
+function divide(n,nn) {
+  return parseFloat(n)/parseFloat(nn)
+}
+function inc(n) {
+  return parseFloat(n)+1
+}
+function dec(n) {
+  return parseFloat(n)-1
+}
+function makeInt(n) {
+  return parseInt(n)
+}
+function p
