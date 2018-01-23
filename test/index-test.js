78fa8141ee3079e6e41bbbab7b7e1f340835924d
petr brown <petrbrown@gmail.com>
2018-01-23 06:05:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,9 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
+console.log('??')
+
+
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
